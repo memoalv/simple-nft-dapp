f28038e99b9c3cccab0b188f3f04639c013c9888
Guillermo Alvarez <guillermo@icalialabs.com>
2021-10-18 21:05:25 -0600

    updated twitter handle

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import twitterLogo from './assets/twitter-logo.svg';
 import './styles/App.css';
 import myEpicNft from './utils/MyEpicNFT.json';
 
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'oyeguillermoso';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 // const OPENSEA_LINK = '';
 // const TOTAL_MINT_COUNT = 50;
@@ -109,7 +109,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
